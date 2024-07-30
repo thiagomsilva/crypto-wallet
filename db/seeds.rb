3612c7905c0ad3822b5e55c1740af99fef786cf3
Thiago Magalhães <thiagomagalhaes2004@hotmail.com>
2024-07-30 17:45:16 -0300

    feat: Add coin seeds and dev environment tasks

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,21 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "Cadastrando moedas..."
+
+Coin.create!(
+  description: "Bitcoin",
+  acronym: "BTC",
+  url_image: "https://static.vecteezy.com/system/resources/previews/008/505/801/original/bitcoin-logo-color-illustration-png.png"
+)
+
+Coin.create!(
+  description: "Ethereum",
+  acronym: "ETH",
+  url_image: "https://cryptologos.cc/logos/ethereum-eth-logo.png"
+)
+
+Coin.create!(
+  description: "Ripple",
+  acronym: "XRP",
+  url_image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQc0fro0-x5gSJZDO5fSWWcM_c56zTAPpr76z2cdWwSxA&s"
+)
+
+puts "Moedas cadastradas com sucesso!"
