fc273d5896342409bb7f1143b223b9f41c173bb0
Thiago Magalhães <thiagomagalhaes2004@hotmail.com>
2024-08-01 22:29:35 -0300

    feat: Add MiningType model, helper, views, and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_26_182154) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_02_011655) do
   create_table "coins", force: :cascade do |t|
     t.string "description"
     t.string "acronym"
     t.string "url_image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "mining_types", force: :cascade do |t|
+    t.string "name"
+    t.string "acronym"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
